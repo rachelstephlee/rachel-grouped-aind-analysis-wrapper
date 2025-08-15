786b465f04945e803581cd5281ff8299385e0263
rachelstephlee <rachel.lee@alleninstitute.org>
2025-08-15 10:03:00 +0000

    now trying the new analysis_util

--- code/analysis_wrapper/run_capsule.py
+++ code/analysis_wrapper/run_capsule.py
@@ -14,9 +14,9 @@ if script_dir in sys.path:
     sys.path.remove(script_dir)
 # sys.path.insert(0,"/root/capsule/")
 
-# sys.path.insert(0, str(Path(__file__).resolve().parent))
+sys.path.insert(0, str(Path(__file__).resolve().parent))
 
-# import analysis_util
+import analysis_util
 
 from analysis_pipeline_utils.metadata import construct_processing_record, docdb_record_exists, write_results_and_metadata
 from analysis_pipeline_utils.analysis_dispatch_model import AnalysisDispatchModel
