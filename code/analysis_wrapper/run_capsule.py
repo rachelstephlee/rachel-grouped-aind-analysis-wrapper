ccc9b12daf4d26e5b631045905601d850ffdae45
rachelstephlee <rachel.lee@alleninstitute.org>
2025-09-26 23:14:52 +0000

    ugh keep missing small changes across the two branches

--- code/analysis_wrapper/run_capsule.py
+++ code/analysis_wrapper/run_capsule.py
@@ -58,7 +58,7 @@ def get_nwb_processed(file_locations, **parameters) -> None:
                          .reset_index(drop=True)
               )
     # only read last N sessions unless daily, weekly plots are requested
-    if "weekly" and "all_sess" not in parameters["plot_types"]:
+    if "weekly" and "all_sess" not in parameters["plot_types"] and parameters["plot_types"] != "":
         df_sess = df_sess.tail(parameters["last_N_sess"])
     
     (df_trials, df_events, df_fip) = co_utils.get_all_df_for_nwb(filename_sessions=df_sess['s3_location'].values, interested_channels = interested_channels)
